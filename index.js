f4ae6371d02fee1f081b81b5e940cd5828adcb3e
Grace <grace941217@gmail.com>
2022-10-01 23:22:13 -0400

    commit tree 98625026ed324d3dd4f824f74e929fada9556c8d

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Guadalupe,", "Ollie,", "Aki,"];
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push("Thank you, " + names[i] + ", for the wonderful " + event + " gift!")
+    }
+    return messages;
+}
+
+
+// function countDownOld() {
+//     let countDown = 10;
+//     for (let i = 0; i <= 10; i++){
+//         console.log(countDown--);
+//     }
+// }
+
+function countDown(count) {
+    for (let i = count; i >= 0; i--){
+        console.log(i);
+    }
+}
